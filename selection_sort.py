26ccf6bddd55656724266da8c5ac51751542d2ca
deino0069 <dheerajdhanju3@gmail.com>
2025-03-05 20:00:11 +0530

    updated function

--- selection_sort.py
+++ selection_sort.py
@@ -1,13 +1,17 @@
-nums = [6,44,32,2,90,5,43]
-
-for i in range(len(nums)):
-    minpos = i
-    for j in range(i,6):
-        if nums[j] < nums[minpos]:
-            minpos = j
+def selectionSorting(nums): #  Function: Used to perform selectionSorting.
+    for i in range(len(nums)):
+        minpos = i
+        for j in range(i,6):
+            if nums[j] < nums[minpos]:
+                minpos = j
 
     
-    nums[i],nums[minpos] = nums[minpos],nums[i]
+        nums[i],nums[minpos] = nums[minpos],nums[i]
+    
+    return nums
 
-print(nums)
-        
+
+
+nums = [6,44,32,2,90,5,43]
+print(selectionSorting(nums))
+
